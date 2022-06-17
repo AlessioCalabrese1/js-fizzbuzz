feaf8afe3e6ed3d597e855c7b8ffacb51ea0bd6a
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-06-17 09:49:09 +0200

    Bonus 1

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+let containerElement = document.getElementById("FizzBuzzDom");
+
+
 for (let index = 1; index <= 100; index++) {
     if ((index % 3) == 0 && (index % 5) != 0) {
         console.log("Fizz");
+        containerElement.append(index);
+        containerElement.innerHTML += "<br>";
     } else if((index % 3) != 0 && (index % 5) == 0){
         console.log("Buzz");
+        containerElement.append("Buzz");
+        containerElement.innerHTML += "<br>";
     } else if((index % 3) == 0 && (index % 5) == 0){
         console.log("FizzBuzz");
+        containerElement.append("FizzBuzz");
+        containerElement.innerHTML += "<br>";
     } else{
         console.log(index);
+        containerElement.append(index);
+        containerElement.innerHTML += "<br>";
     }
 }
