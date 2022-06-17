4da59137239e27d4f6333d4c194ded15b0a09d8c
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-06-17 10:17:46 +0200

    Bonus 2

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 let containerElement = document.getElementById("FizzBuzzDom");
-
+let childDiv = document.createElement("div");
 
 for (let index = 1; index <= 100; index++) {
     if ((index % 3) == 0 && (index % 5) != 0) {
         console.log("Fizz");
-        containerElement.append(index);
+        childDiv.classList = ("fizzColor col-2 text-center d-flex align-items-center justify-content-center m-3 heightBlock");
+        childDiv.innerHTML = "Fizz";
+        containerElement.append(childDiv);
         containerElement.innerHTML += "<br>";
     } else if((index % 3) != 0 && (index % 5) == 0){
         console.log("Buzz");
-        containerElement.append("Buzz");
+        childDiv.classList = ("buzzColor col-2 text-center d-flex align-items-center justify-content-center m-3 heightBlock");
+        childDiv.innerHTML = "Buzz";
+        containerElement.append(childDiv);
         containerElement.innerHTML += "<br>";
     } else if((index % 3) == 0 && (index % 5) == 0){
         console.log("FizzBuzz");
-        containerElement.append("FizzBuzz");
+        childDiv.classList = ("fizzBuzzColor col-2 text-center d-flex align-items-center justify-content-center m-3 heightBlock");
+        childDiv.innerHTML = "FizzBuzz";
+        containerElement.append(childDiv);
         containerElement.innerHTML += "<br>";
     } else{
         console.log(index);
-        containerElement.append(index);
+        childDiv.classList = ("number col-2 text-center d-flex align-items-center justify-content-center m-3 heightBlock");
+        childDiv.innerHTML = index;
+        containerElement.append(childDiv);
         containerElement.innerHTML += "<br>";
     }
 }
